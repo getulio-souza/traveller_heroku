b10468cc85b67181cefcd6b224889bf7679d86f7
Getúlio Souza <getuliosouza@live.com>
2022-09-04 22:17:14 -0300

    link api google incluída

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
-L.mapquest.key = 'KEY';
+const encuentrame = () => {
+    //todo o processo de localização
 
-// 'map' refers to a <div> element with the ID map
-L.mapquest.map('map', {
-  center: [37.7749, -122.4194],
-  layers: L.mapquest.tileLayer('map'),
-  zoom: 12
-});
+    const success = (position) => {
+        const divCoordenadas = document.getElementById("coordenadas");
+        const divMapa = document.getElementById("mapa");
+        console.log(position);
+        const { latitude , longitude } = position.coords;
+        console.log(latitude, longitude);
+
+        divCoordenadas.innerHTML = `lat: ${latitude}, lng: ${longitude}`;
+    
+        //mapa
+        // https://www.mapquestapi.com/staticmap/v5/map?key=KEY&locations=LOCATION&size=WIDTH,HEIGHT
+        const mapa = new Image();
+        mapa.src = `https://www.mapquestapi.com/staticmap/v5/map?key=xzaNvERnuyVMzk0l3PvjLp3M6x797aet&locations=${latitude},${longitude}&size=400,300`;
+        divMapa.appendChild(mapa);
+    };
+
+    const errorPosicion = (error) => {
+        const divCoordenadas = document.getElementById("coordenadas");
+        divCoordenadas.innerHTML = `Erro ao obter localização:<br> Error ${error.code}: ${error.message}`;
+    };
+
+    navigator.geolocation.getCurrentPosition( success , errorPosicion );
+};
+
+const verificarGeo = () => {
+    if(!navigator.geolocation){
+    //não suporta localização
+    const divCoordenadas = document.getElementById("coordenadas");
+    divCoordenadas.innerHTML ="<p>localização não suportada pelo navegador</>";
+    return;
+    }
+ //Se suportar, chamar a localização
+  encuentrame();
+};
+
+verificarGeo();
